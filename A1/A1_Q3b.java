97c0a417f2d035935896021b704458f589ea3b5d
Erik Cupsa <2003eec@gmail.com>
2024-02-14 01:20:17 -0500

    Completed assignment 1

--- A1/A1_Q3b.java
+++ A1/A1_Q3b.java
@@ -3,7 +3,26 @@ import java.util.*;
 public class A1_Q3b {
 
 	public static int protocol_3b(int[] arrangement) {
-		return 0;
+		HashSet<Integer> uniqueCourses = new HashSet<>(); //set for current list of unique courses
+		//initializing variables
+		int left = 0;
+		int right = 0;
+		int maxLength = 0;
+
+		while (right < arrangement.length) { //looping through array
+            if (!uniqueCourses.contains(arrangement[right])) { //unique course found
+                uniqueCourses.add(arrangement[right]); //add course to list
+                right++; //extend window right
+                int currentLength = right - left; //calculate current length and update max
+                maxLength = Math.max(maxLength, currentLength);
+            } else {
+                // Duplicate exam found, so remove the left most exam
+                uniqueCourses.remove(arrangement[left]);
+                left++; //shift the window left
+            }
+        }
+
+        return maxLength; //return max length of window
 	}
 
 }
