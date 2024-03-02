64a3523aa35b07a242a73e14cc7ad7f35cedc4b8
Erik Cupsa <2003eec@gmail.com>
2024-03-02 00:26:17 -0500

    Completed Q2 A2

--- A2/A2_Q2.java
+++ A2/A2_Q2.java
@@ -4,12 +4,35 @@ import java.util.Scanner;
 public class A2_Q2 {
 
 	public static String mod_fibonacci(int N, BigInteger K) {
-		
+		BigInteger[] fibonacciNums = new BigInteger[10000];
+		fibonacciNums[0] = BigInteger.valueOf(0);
+		fibonacciNums[1] = BigInteger.valueOf(1);
+		fibonacciNums[2] = BigInteger.valueOf(1);
+
+		for (int i = 3; i <= N; i++){
+			fibonacciNums[i] = fibonacciNums[i-2].add(fibonacciNums[i-1]);
+		}
+		return calculateFibonacci(N, K, fibonacciNums);
+	}
+
+	private static String calculateFibonacci(int N, BigInteger K, BigInteger[] fibonacciNums){
+		if (N == 1) return "X";
+		if (N == 2) return "Y";
+		BigInteger currFib = fibonacciNums[N-2];
+		if(K.compareTo(currFib)>0){
+			return calculateFibonacci(N-1, K.subtract(currFib), fibonacciNums);
+		}
+		else{
+			return calculateFibonacci(N-2, K, fibonacciNums);
+		}
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		BigInteger k = scanner.nextBigInteger();
+		String result = mod_fibonacci(n, k);
+		System.out.println(result);
 	}
 
 }
