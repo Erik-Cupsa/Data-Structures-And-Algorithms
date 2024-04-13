3786bcd09b4e99de9bf90dcab8eb665067d5cf05
Erik Cupsa <2003eec@gmail.com>
2024-04-13 18:05:00 -0400

    Completed Q2

--- A3/Q2/A3Q2.java
+++ A3/Q2/A3Q2.java
@@ -26,8 +26,67 @@ public class A3Q2 {
 		System.out.println(Arrays.toString(g.issue2.toArray()));
 	}
 
-	public static void F1(Graph g){
+	// public static HashSet<Integer> dfs(Graph g, int start) {
+	// 	HashSet<Integer> visited = new HashSet<>();
+	// 	Stack<Integer> stack = new Stack<>();
+	// 	stack.push(start);
+	// 	while (!stack.isEmpty()){
+	// 		int current = stack.pop();
+	// 		if (!visited.contains(current)) {
+	// 			visited.add(current);
+	// 			for (int neighbour : g.adj.get(current)) {
+	// 				stack.push(neighbour);
+	// 			}
+	// 		}
+	// 	}
+	// 	return visited;
+	// }
+public static void dfs(Hashtable<Integer, LinkedList<Integer>> adj, int node, HashSet<Integer> visited) {
+		if (!visited.contains(node)) {
+			visited.add(node);
+			processNeighbors(adj, node, visited);
+		}
+	}
 
+	private static void processNeighbors(Hashtable<Integer, LinkedList<Integer>> adj, int node, HashSet<Integer> visited) {
+		if (adj.get(node) == null) {
+			return;
+		}
+		else{
+			for (int neighbor : adj.get(node)) if (!visited.contains(neighbor))dfs(adj, neighbor, visited);
+		}
+	}
+	
+	public static Graph transpose(Graph g) {
+		Graph transposed = new Graph(g.num_nodes);
+		for (int vertex : g.adj.keySet()) {
+			LinkedList<Integer> neighbours = g.adj.get(vertex);
+			for (int neighbour : neighbours) {
+				LinkedList<Integer> list = transposed.adj.computeIfAbsent(neighbour, k -> new LinkedList<>());
+				list.add(vertex);
+			}
+		}
+		return transposed;
+	}
+
+	public static void F1(Graph g){
+		HashSet<Integer> reachableFromIsland = new HashSet<>();
+		HashSet<Integer> canReachIsland = new HashSet<>();
+
+		dfs(g.adj, 0, reachableFromIsland);
+		Graph transposed = transpose(g);
+		dfs(transposed.adj, 0, canReachIsland);
+
+		for (Integer point : g.order) {
+			if (!reachableFromIsland.contains(point)) {
+				g.issue2.add(point);
+			}
+		}
+		for (Integer point : g.order) {
+			if (!canReachIsland.contains(point)) {
+				g.issue1.add(point);
+			}
+		}
 	}
 }
 
@@ -56,6 +115,3 @@ class Graph{
 		return this.order;
 	}	
 }
-
-
-
